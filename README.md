33a9ff06af70992f8c3ac966481573efe9226e7e
Benjamin Alpert <bja282@nyu.edu>
2017-08-06 16:15:39 -0400

    Adding Readme and Requirements files. Updating project file with more conditionals.

---
+++ README.md
@@ -0,0 +1,7 @@
+This is an application for logging and viewing my fitness data.
+
+Start by installing required packages. If your system is running Python 2, please run the following:
+pip install -r requirements.txt
+
+In the event your system is running Python 3, please run the following instead:
+pip3 install -r requirements.txt
--- project.py
+++ project.py
@@ -16,7 +16,7 @@ for tag in xml_tags:
         "status": tag.find('status').string,
         "date": tag.find('Date').string,
         "time": tag.find('Time').string,
-        "desc": BeautifulSoup(tag.find('text').prettify(formatter=None)),
+        "desc": BeautifulSoup(tag.find('text').prettify(formatter=None), 'lxml'),
         "announcements": ""
     }
     lines.append(line)
@@ -27,26 +27,66 @@ def soupertrain(bowl):
     if bowl['status'].lower() != "Good Service".lower():
         try:
             leansoup = bowl['desc'].find_all('a')
+            if bowl['status'].lower() == "delays":
+                try:
+                    paprika.append("The "+bowl['name']+" line is currently running with delays.")
+                except:
+                    pass
             for x in range(0, len(leansoup)):
                 if leansoup[x].has_attr('class') and leansoup[x].has_attr('onclick'):
                     paprika.append(leansoup[x].text)
         except(ValueError, IndexError):
             pass
+    elif bowl['status'].lower() == "delays":
+        try:
+            paprika.append("The "+bowl['name']+" line is currently running with delays.")
+        except:
+            pass
     else:
         paprika.append(bowl['status'].title())
     bowl['announcements']=paprika
-    #print(paprika)
 
 for line in lines:
     soupertrain(line)
     print("+", line['name']+", Service Status: "+line['status'])#+" "+line['desc'])
 
+def numberofchanges(announcements):
+    count = 0
+
+    if count>0:
+        print(str(count))
+    else:
+        print("No current service changes or updates.")
+
+def countcheck(count):
+    if count>0 and count<2:
+        return(str(count)+" current service change or update.")
+    elif count>1:
+        return(str(count)+" current service changes or updates.")
+    else:
+        return("No current service changes or updates.")
+
 def line_lookup(name):
   matches = []
+  count = 0
   for line in lines:
       if line["name"].upper() == name.upper():
           matches.append(line)
-  print("Line: "+matches[0]["name"]+"\n"+"Service Status: ",matches[0]["desc"])
+  if matches[0]['announcements'][0].lower() == "good service".lower():
+      count = 0
+  else:
+      count=len(matches[0]['announcements'])
+  #code.interact(local=locals())
+  print("\n\nLine: "+matches[0]["name"]+"\n"+"Service Status: "+countcheck(count))
+  try:
+      for match in matches[0]["announcements"]:
+          if count>0:
+              print ("+ ", match)
+          else: pass
+  except(ValueError, IndexError):
+      pass
+  print("\n")
+
 
 #MENU
 while True:
@@ -63,4 +103,6 @@ while True:
         if service_prompt != "done":
             line_lookup(service_prompt)
         else:
-            break
+            service_prompt = input("Sorry, didn't recognize that. Try again? ")
+            if service_prompt != "done":
+                line_lookup(service_prompt)
